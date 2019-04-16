6caea5c9d056573baf5a93186676d7375adc24ba
Emil Luusua <emilluusua@gmail.com>
2019-04-16 20:31:10 +0800

    Completed function for connecting two points by a road

--- Assets/Scripts/CreateRoadsScript.cs
+++ Assets/Scripts/CreateRoadsScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class CreateRoadsScript : MonoBehaviour
 { 
     public GameObject roadSegment;
-    private float segmentLength = 5.0F;
+    private float segmentLength;
     private List<GameObject> roads;
 
     // Start is called before the first frame update
     void Start()
     {
         roads = new List<GameObject>();
+        segmentLength = roadSegment.GetComponent<Renderer>().bounds.size[0];
     }
 
     // Update is called once per frame
@@ -20,26 +21,58 @@ public class CreateRoadsScript : MonoBehaviour
         {
             Destroy(road);
         }
-        roads = new List<GameObject>();
 
+        roads = new List<GameObject>();
         GameObject[] houses = GameObject.FindGameObjectsWithTag("House");
 
-        for (int i = 0; i < houses.Length; ++i)
+        if (houses.Length >= 2)
         {
-            for (int j = i + 1; j < houses.Length; ++j)
-            {
-                ConnectHouses(houses[i], houses[j]);
-            }
+            ConnectHouses(houses[0].transform, houses[1].transform);
         }
 
     }
 
-    // Connects two houses by roads
-    void ConnectHouses(GameObject h1, GameObject h2)
+    // Finds the minimum spanning tree in the fully connected graph between nodes
+    void MinimumSpanningTree(GameObject[] nodes)
     {
-        Vector3 p1 = h1.transform.position;
-        Vector3 p2 = h2.transform.position;
-        float dist = Vector3.Distance(p1, p2);
-        int numOfSegments = (int)(dist / segmentLength);
+        foreach (GameObject node in nodes)
+        {
+            Debug.Log("Hej");
+        }
+    }
+
+    // Connects two houses by roads
+    void ConnectHouses(Transform h1, Transform h2)
+    {
+        Vector3 dir = h2.position - h1.position;
+        int numOfSegments = (int) Mathf.Ceil(dir.magnitude / segmentLength);
+
+        if (numOfSegments > 1)
+        {
+            float spacing = (dir.magnitude - segmentLength) / (numOfSegments - 1);
+
+            for (int i = 0; i < numOfSegments; ++i)
+            {
+                GameObject road = Instantiate(
+                    roadSegment,
+                    h1.position + (segmentLength / 2 + i * spacing) * dir / dir.magnitude,
+                    Quaternion.LookRotation(dir, h1.up));
+
+                road.transform.Rotate(new Vector3(-90, 0, 0));
+                roads.Add(road);
+            }
+        }
+        else
+        {
+            GameObject road = Instantiate(
+                roadSegment,
+                h1.position + dir / 2,
+                Quaternion.LookRotation(dir, h1.up));
+
+            road.transform.Rotate(new Vector3(-90, 0, 0));
+            roads.Add(road);
+        }
+
+
     }
 }
