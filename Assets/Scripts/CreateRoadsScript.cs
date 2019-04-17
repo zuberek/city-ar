039c7892e16178e1f7f616ae9c75fa0ffba9bf08
Emil Luusua <emilluusua@gmail.com>
2019-04-17 17:05:15 +0800

    Switched out the car asset for a cooler one, and made cars drive on the roads

--- Assets/Scripts/CreateRoadsScript.cs
+++ Assets/Scripts/CreateRoadsScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class CreateRoadsScript : MonoBehaviour
 { 
     public GameObject roadSegment;
+    public GameObject carPrefab;
     private float segmentLength;
     private List<GameObject> roads = new List<GameObject>();
+    private List<Car> cars = new List<Car>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,25 +29,59 @@ public class CreateRoadsScript : MonoBehaviour
         if (houses.Length > 1)
         {
             var housesToConnect = MinimumSpanningTree(houses);
+            var connectedHouses = new HashSet<(GameObject, GameObject)>();
+
+            // Update car positions and connect houses of those cars
+            for (int i = cars.Count - 1; i >= 0; --i)
+            {
+                Car car = cars[i];
+                bool foundRoad = false;
+
+                foreach (var housePair in housesToConnect)
+                {
+                    if ((housePair.Item1 == car.destination && housePair.Item2 == car.source) ||
+                        (housePair.Item2 == car.destination && housePair.Item1 == car.source))
+                    {
+                        ConnectHouses(housePair.Item1.transform, housePair.Item2.transform);
+                        connectedHouses.Add(housePair);
+
+                        car.Step();
+                        foundRoad = true;
+                        break;
+                    }
+                }
+
+                if (!foundRoad)
+                {
+                    // This car no longer has a road to drive on :(
+                    cars.Remove(car);
+                    Destroy(car.car);
+                }
+            }
+
+            // Connect houses with no car, and make them a car
+            housesToConnect.ExceptWith(connectedHouses);
             foreach (var housePair in housesToConnect)
             {
-                ConnectHouses(housePair.Item1, housePair.Item2);
+                Car car = new Car(carPrefab, housePair.Item1, housePair.Item2);
+                cars.Add(car);
+                ConnectHouses(housePair.Item1.transform, housePair.Item2.transform);
             }
         }
     }
 
     // Finds the minimum spanning tree in the fully connected graph between objs
-    HashSet<(Transform, Transform)> MinimumSpanningTree(GameObject[] objs)
+    HashSet<(GameObject, GameObject)> MinimumSpanningTree(GameObject[] objs)
     {
-        Transform root = objs[0].transform;
-        var edgesToChooseFrom = new List<(Transform, Transform)>();
-        var edges = new HashSet<(Transform, Transform)>();
+        GameObject root = objs[0];
+        var edgesToChooseFrom = new List<(GameObject, GameObject)>();
+        var edges = new HashSet<(GameObject, GameObject)>();
         var edgeComparer = new EdgeComparer();
 
         // Fill edgesToChooseFrom with edges from the root
         for (int i = 1; i < objs.Length; ++i)
         {
-            Transform h = objs[i].transform;
+            GameObject h = objs[i];
             edgesToChooseFrom.Add((h, root));
         }
 
@@ -102,15 +138,54 @@ public class CreateRoadsScript : MonoBehaviour
             road.transform.Rotate(new Vector3(-90, 0, 0));
             roads.Add(road);
         }
-
-
     }
 
-    private class EdgeComparer : IComparer<(Transform, Transform)>
+    private class EdgeComparer : IComparer<(GameObject, GameObject)>
     {
-        public int Compare((Transform, Transform) e1, (Transform, Transform) e2)
+        public int Compare((GameObject, GameObject) e1, (GameObject, GameObject) e2)
         {
-            return Vector3.Distance(e1.Item1.position, e1.Item2.position).CompareTo(Vector3.Distance(e2.Item1.position, e2.Item2.position));
+            return Vector3.Distance(e1.Item1.transform.position, e1.Item2.transform.position).CompareTo(Vector3.Distance(e2.Item1.transform.position, e2.Item2.transform.position));
+        }
+    }
+
+    private class Car
+    {
+        public GameObject car;
+        public GameObject destination;
+        public GameObject source;
+        public float progress = 0.0F;
+
+        public Car(GameObject carPrefab, GameObject destinationObj, GameObject sourceObj)
+        {
+            destination = destinationObj;
+            source = sourceObj;
+            Vector3 dir = destination.transform.position - source.transform.position;
+            car = Instantiate(
+                carPrefab,
+                source.transform.position,
+                Quaternion.LookRotation(dir, source.transform.up));
+        }
+
+        public void Step()
+        {
+            progress += 0.01F;
+
+            if (progress >= 1.0F)
+            {
+                SwitchDirection();
+            }
+
+            Vector3 dir = destination.transform.position - source.transform.position;
+            car.transform.position = source.transform.position + dir * progress;
+            car.transform.rotation = Quaternion.LookRotation(dir, source.transform.up);
+        }
+
+        private void SwitchDirection()
+        {
+            GameObject temp = destination;
+            destination = source;
+            source = temp;
+            progress = 0.0F;
         }
     }
 }
